56b54229198f22731e80096342d194a7fd792dba
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2026-09-14 17:27:47 +0500

    [*] removed line numbering in .txt

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# cuda_print
 Plugin for CudaText: printing text/code/etc via the browser
--- __init__.py
+++ __init__.py
@@ -12,23 +12,32 @@ class Command:
 		return
 
 	def run(self):
-		fn = Path(tempfile.mkdtemp()) / os.path.basename(ed.get_filename() + '.txt')
+		text = ed.get_text_sel() if ed.get_text_sel() else ed.get_text_all()
+
+		suffix_txt = '.txt'
+		if ed.get_filename().endswith(suffix_txt):
+			text_print = text
+			fn_ = os.path.basename(ed.get_filename())
+		else:
+			text_ = ''
+			i = 0
+			for line in text.splitlines():
+				i = i + 1
+				text_ += '[' + str(i) + ']' + "\t" + line + "\n"
+			text_print = text_
+			fn_ = os.path.basename(ed.get_filename() + suffix_txt)
+
+		fn = Path(tempfile.mkdtemp()) / fn_
 		if not fn:
 			msg_box(_('Cannot preview untitled tab'), MB_OK)
 			return
 
-		text = ed.get_text_sel() if ed.get_text_sel() else ed.get_text_all()
-		code = ''
-		i = 0
-		for line in text.splitlines():
-			i = i + 1
-			code += '[' + str(i) + ']' + "\t" + line + "\n"
 		with open(fn, 'w') as f:
-			f.write(code)
+			f.write(text_print)
 
 		if not os.path.isfile(fn):
 			msg_status(_('Cannot open file: ')+fn)
 			return
 
 		webbrowser.open_new_tab(str(fn))
-		msg_box(_("This code is open in the browser.\nPress Ctrl+P there to print."), MB_OK)
+		msg_box(_("Text is open via the browser.\nPress Ctrl+P there to print."), MB_OK)
